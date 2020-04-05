5bd82f8de659cb5187b9bfa9c005f6dc7c2efa83
Weronika <werkadd@gmail.com>
2020-04-05 22:40:21 +0200

    git ignore added and functions moved to separate file

---
+++ text_prepare.py
@@ -0,0 +1,26 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from bs4 import BeautifulSoup
+
+def html_docode(text):
+    return [BeautifulSoup(article, "html").text for article in text]# HTML decoding. BeautifulSoup's text attribute will return a string stripped of any HTML tags and metadata.
+
+
+def clean_text(text):
+    REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;\\n]')
+    BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+    STOPWORDS = set(stopwords.words('english'))
+    cleaned_text = [article.lower() for article in text]# lowercase text
+    cleaned_text = [REPLACE_BY_SPACE_RE.sub(' ', article)  for article in cleaned_text] # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
+    cleaned_text = [BAD_SYMBOLS_RE.sub('', article) for article in cleaned_text] # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
+    return [' '.join(word for word in article.split() if word not in STOPWORDS) for article in cleaned_text] 
+    
+def stemm_text(text):
+    stemmer = PorterStemmer()
+    return [' '.join([stemmer.stem(word) for word in review.split()]) for review in text]
+
+def lemmatize_text(text):
+    lemmatizer = WordNetLemmatizer()
+    return [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in text]
