551f4d8fb4a0596cc9d986ea5db5bef4c7abb11d
Weronika Dranka <werkadd@gmail.com>
2020-05-10 15:50:21 +0200

    TO DO finish word mover + run in parallel + add abstraction

--- text_prepare.py
+++ text_prepare.py
@@ -15,7 +15,8 @@ def clean_text(text):
     cleaned_text = [article.lower() for article in text]# lowercase text
     cleaned_text = [REPLACE_BY_SPACE_RE.sub(' ', article)  for article in cleaned_text] # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
     cleaned_text = [BAD_SYMBOLS_RE.sub('', article) for article in cleaned_text] # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
-    return [' '.join(word for word in article.split() if word not in STOPWORDS) for article in cleaned_text] 
+    cleaned_text = [re.sub(r'[0-9]+','', article) for article in cleaned_text] #remove numbers
+    return [' '.join(word for word in article.split() if (word not in STOPWORDS and len(word) >3)) for article in cleaned_text] 
     
 def stemm_text(text):
     stemmer = PorterStemmer()
